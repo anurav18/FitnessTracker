188ad110b08e1de9ca547b12c1b0499bf9d527c8
Anu Rav <anusharavva@gmail.com>
2021-01-13 16:41:05 -0800

    Updated the schema based on the seed data

--- Develop/models/workOut.js
+++ Develop/models/workOut.js
@@ -3,61 +3,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workOutSchema = new Schema({
-  exerciseType: {
-    type: String,
-    trim: true,
-    required: "Exercise type is Required"
-  },
 
-  distance: {
-    type: Number,
-    trim: true,
-    required: "Distance is Required"
+  day:{
+    type: Date,
+    default: Date.now
   },
-
-  duration: {
-    type: Number,
-    trim: true,
-    required: "Duration is Required"
-  },
-
-  cardioName: {
-    type: String,
-    trim: true,
-    required: "cardio Name is Required"
-  },
-
-  exerciseName: {
-    type: String,
-    trim: true,
-    required: "exercise Name is Required"
-  },
-
-  weight: {
-    type: Number,
-    trim: true,
-    required: "Weight(lbs) is Required"
-  },
-
-  sets: {
-    type: Number,
-    trim: true,
-    required: "Number of sets performed is Required"
-  },
-  reps: {
-    type: Number,
-    trim: true,
-    required: "Number of reps performed is Required"
-  },
-  resistanceDuration: {
-    type: Number,
-    trim: true,
-    required: "Duration is Required"
-  }
- 
+  exercises: [
+    {
+      type: String,
+      name: String,
+      duration: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+      distance: Number
+    }
+  ]
 });
-
-
 const workOut = mongoose.model("workOut", workOutSchema);
 
 module.exports = workOut;
